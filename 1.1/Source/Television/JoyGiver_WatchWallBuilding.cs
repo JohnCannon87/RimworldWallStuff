7843487822d93a051f4d128ea1c18db71ff5d37f
John Cannon <john.arc.cannon@gmail.com>
2020-02-17 18:01:58 +0000

    Updated for V1.1

---
+++ 1.1/Source/Television/JoyGiver_WatchWallBuilding.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace WallStuff
+{
+    class JoyGiver_WatchWallBuilding : JoyGiver_WatchBuilding
+    {
+        private static List<Thing> tmpCandidates = new List<Thing>();
+
+        public override Job TryGiveJob(Pawn pawn)
+        {
+            Thing thing = this.FindBestGame(pawn, false, IntVec3.Invalid);
+            if (thing != null)
+            {
+                return this.TryGivePlayJob(pawn, thing);
+            }
+            return null;
+        }
+
+        private Thing FindBestGame(Pawn pawn, bool inBed, IntVec3 partySpot)
+        {
+            tmpCandidates.Clear();
+            this.GetSearchSet(pawn, tmpCandidates);
+            if (tmpCandidates.Count == 0)
+            {
+                return null;
+            }
+            Predicate<Thing> predicate = (Thing t) => this.CanInteractWith(pawn, t, inBed);
+            if (partySpot.IsValid)
+            {
+                Predicate<Thing> oldValidator = predicate;
+                predicate = ((Thing x) => GatheringsUtility.InGatheringArea(x.Position, partySpot, pawn.Map) && oldValidator(x));
+            }
+            IntVec3 position = pawn.Position;
+            Map map = pawn.Map;
+            List<Thing> searchSet = tmpCandidates;
+            Predicate<Thing> validator = predicate;
+            Thing thing = searchSet[0];
+            Thing result = GenClosest.ClosestThing_Global(position, searchSet, 9999f, validator, null);
+            tmpCandidates.Clear();
+            return result;
+        }
+
+        protected override void GetSearchSet(Pawn pawn, List<Thing> outCandidates)
+        {
+            outCandidates.Clear();
+            if (this.def.thingDefs == null)
+            {
+                return;
+            }
+            if (this.def.thingDefs.Count == 1)
+            {
+                outCandidates.AddRange(pawn.Map.listerThings.ThingsOfDef(this.def.thingDefs[0]));
+            }
+            else
+            {
+                for (int i = 0; i < this.def.thingDefs.Count; i++)
+                {
+                    ThingDef thingDef = this.def.thingDefs[i];
+                    List<Thing> thingsFound = pawn.Map.listerThings.ThingsOfDef(thingDef);
+                    outCandidates.AddRange(thingsFound);
+                }
+            }
+        }
+
+        protected override bool CanInteractWith(Pawn pawn, Thing t, bool inBed)
+        {
+            if (!base.CanInteractWith(pawn, t, inBed))
+            {
+                return false;
+            }
+            if (inBed)
+            {
+                Building_Bed bed = pawn.CurrentBed();
+                if (t.def.building.isEdifice){
+                    return WatchBuildingUtility.CanWatchFromBed(pawn, bed, t);
+                } else
+                {
+                    return WatchWallBuildingUtility.CanWatchFromBed(pawn, bed, t);
+                }
+            }
+            return true;
+        }
+
+        protected override Job TryGivePlayJob(Pawn pawn, Thing t)
+        {
+            IntVec3 c;
+            Building t2;
+            if (t.def.building.isEdifice)
+            {
+                if (!WatchWallBuildingUtility.TryFindBestWatchCell(t, pawn, this.def.desireSit, out c, out t2))
+                {
+                    return null;
+                }
+            } else
+            {
+                if (!WatchBuildingUtility.TryFindBestWatchCell(t, pawn, this.def.desireSit, out c, out t2))
+                {
+                    return null;
+                }
+            }
+            return new Job(this.def.jobDef, t, c, t2);
+        }
+    }
+}
---
+++ 1.1/Source/Temperature/Building_MediumCooler.cs
@@ -0,0 +1,133 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using WallStuff;
+
+namespace WallStuff
+{
+    public class Building_MediumCooler : Building_TempControl, IWallAttachable
+    {
+        public Thing glower;
+
+        private IntVec3 vecNorth;
+        private Room roomNorth;
+        private CompMyGlower compGlower;
+        private bool isWorking;
+        private bool wasLit;
+
+        private bool WorkingState
+        {
+            get { return isWorking; }
+            set
+            {
+                isWorking = value;
+
+                if (compPowerTrader == null || compTempControl == null)
+                {
+                    return;
+                }
+                if (isWorking)
+                {
+                    compPowerTrader.PowerOutput = -compPowerTrader.Props.basePowerConsumption;
+                }
+                else
+                {
+                    compPowerTrader.PowerOutput = -compPowerTrader.Props.basePowerConsumption*
+                                                  compTempControl.Props.lowPowerConsumptionFactor;
+                }
+
+                compTempControl.operatingAtHighPower = isWorking;
+            }
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            vecNorth = Position + IntVec3.North.RotatedBy( Rotation );
+
+            glower = GenSpawn.Spawn( ThingDef.Named( "WallStuff_CoolerGlower" ), vecNorth, map);
+            ((Building_CoolerGlower) glower).Reinit( this );
+            compGlower = glower.TryGetComp< CompMyGlower >();
+            //compGlower.Lit = false;
+        }
+
+        public override void Destroy( DestroyMode mode = DestroyMode.Vanish )
+        {
+            glower.Destroy();
+            base.Destroy( mode );
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (compPowerTrader.PowerOn && !wasLit)
+            {
+                compGlower.UpdateLit(true);
+                wasLit = true;
+            }
+            else if (!compPowerTrader.PowerOn && wasLit)
+            {
+                compGlower.UpdateLit(false);
+                wasLit = false;
+            }
+
+            if (!this.IsHashIntervalTick( 60 ))
+            {
+                return;
+            }
+            if (!Validate())
+            {
+                WorkingState = false;
+                return;
+            }
+
+            ControlTemperature();
+        }
+
+        protected virtual bool Validate()
+        {
+            if (vecNorth.Impassable(this.Map))
+            {
+                return false;
+            }
+
+            roomNorth = vecNorth.GetRoom(this.Map);
+            if (roomNorth == null)
+            {
+                return false;
+            }
+
+            return (compPowerTrader == null || compPowerTrader.PowerOn);
+        }
+
+        private void ControlTemperature()
+        {
+            var temperature = roomNorth.Temperature;
+            float energyMod;
+            if (temperature < 20f)
+            {
+                energyMod = 1f;
+            }
+            else
+            {
+                energyMod = temperature > 120f
+                    ? 0f
+                    : Mathf.InverseLerp( 120f, 20f, temperature );
+            }
+            var energyLimit = WallStuffSettings.coolerPower*energyMod*4.16666651f;
+            var hotAir = GenTemperature.ControlTemperatureTempChange( vecNorth, this.Map, energyLimit,
+                                                                      compTempControl.targetTemperature );
+
+            var hotIsHot = !Mathf.Approximately( hotAir, 0f );
+            if (hotIsHot)
+            {
+                roomNorth.Group.Temperature += hotAir;
+                WorkingState = true;
+            }
+            else
+            {
+                WorkingState = false;
+            }
+        }
+    }
+}
